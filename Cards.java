145971eb9465487e6452b15062069aac073abe3a
GoldAu <picard33470@sas.edu.sg>
2016-10-18 12:02:10 +0800

    Update Cards.java

--- Cards.java
+++ Cards.java
@@ -43,7 +43,7 @@ public class Cards{
 			return faceCard;
 	}
 	public int getCardNumber(){
-			return cardNumber;
+		return cardNumber;
 	}
 	public boolean isAFaceCard(){
 		return faceCardNumber;
