29f5ef9fb758f09d1f8bdf4dc475bc5f68d9b4e9
GoldAu <picard33470@sas.edu.sg>
2016-10-18 11:58:42 +0800

    Update BlackJack0.java

--- BlackJack0.java
+++ BlackJack0.java
@@ -5,6 +5,7 @@ public class BlackJack0{
 		final int suits = 4;
 		int drawCardsSuits = cards/suits;
 		Scanner sc = new Scanner(System.in);
+		boolean gameOver = false;
 		
 		System.out.println("How many decks would you like to play with?");
 		
@@ -12,38 +13,66 @@ public class BlackJack0{
 		Cards[] deckCards = new Cards[decks * cards];
 		int suitCards = deckCards.length / 4;
 		for(int i = 0; i < suitCards; i++){ //Initialize first suit of cards
-			deckCards[i] = new Cards((i % 10) + 1, 0);
+			deckCards[i] = new Cards((i % 13) + 1, 0);
 		}
 		for(int i = 0; i < suitCards; i++){
-			deckCards[i + suitCards] = new Cards((i % 10) + 1, 1);
+			deckCards[i + suitCards] = new Cards((i % 13) + 1, 1);
 		}
 		for(int i = 0; i < suitCards; i++){
-			deckCards[i + 2 * suitCards] = new Cards((i % 10) + 1, 2);
+			deckCards[i + 2 * suitCards] = new Cards((i % 13) + 1, 2);
 		}
 		for(int i = 0; i < suitCards; i++){
-			deckCards[i + 3*suitCards] = new Cards((i % 10) + 1, 3);
+			deckCards[i + 3 * suitCards] = new Cards((i % 13) + 1, 3);
 		}
 		
-		System.out.println("How many players are there? (Maximum of 5)"); /*Technically doesn't have to be 5,
-										    but for the game's sake I made it five.
-										  */
-		
+		System.out.println("How many players are there? (Maximum of 5)");
 		int players = sc.nextInt();
 		while(players > 5 || players < 1){
-			System.out.println("Max amount of players is 5! And no negative numbers!");
+			System.out.println("Maximum of 5 and at least one player");
 			players = sc.nextInt();
 		}
 		Player[] x = new Player[players];
 		for(int i = 0; i < x.length; i++){
 			int drawRand = (int)(Math.random() * deckCards.length);
 			int drawRand2 = (int)(Math.random() * deckCards.length);
-			while(deckCards[drawRand] == null || drawRand == drawRand2)
+			while(deckCards[drawRand] == null || deckCards[drawRand] == deckCards[drawRand2])
 				drawRand = (int)(Math.random() * deckCards.length);
-			while(deckCards[drawRand2] == null || drawRand == drawRand2)
+			while(deckCards[drawRand2] == null || deckCards[drawRand] == deckCards[drawRand2])
 				drawRand2 = (int)(Math.random() * deckCards.length);
 			x[i] = new Player(deckCards[drawRand].getCardNumber(), 
 					deckCards[drawRand2].getCardNumber());
-			System.out.println("Player " + (i + 1) + " has a "
+			if(deckCards[drawRand2].isAFaceCard() && deckCards[drawRand].isAFaceCard()){
+				System.out.println("Player " + (i + 1) + " has a "
+						+ deckCards[drawRand].getFaceCard() + " of "
+						+ deckCards[drawRand].getSuit() + " and a "
+						+ deckCards[drawRand2].getFaceCard() + " of "
+						+ deckCards[drawRand2].getSuit() + " for a total of "
+						+ (deckCards[drawRand].getCardNumber() + deckCards[drawRand2].getCardNumber()));
+				deckCards[drawRand] = null;
+				deckCards[drawRand2] = null;
+			}
+			else if(deckCards[drawRand].isAFaceCard()){
+				System.out.println("Player " + (i + 1) + " has a "
+						+ deckCards[drawRand].getFaceCard() + " of "
+						+ deckCards[drawRand].getSuit() + " and a "
+						+ deckCards[drawRand2].getCardNumber() + " of "
+						+ deckCards[drawRand2].getSuit() + " for a total of "
+						+ (deckCards[drawRand].getCardNumber() + deckCards[drawRand2].getCardNumber()));
+				deckCards[drawRand] = null;
+				deckCards[drawRand2] = null;
+			}
+			else if(deckCards[drawRand2].isAFaceCard()){
+				System.out.println("Player " + (i + 1) + " has a "
+						+ deckCards[drawRand].getCardNumber() + " of "
+						+ deckCards[drawRand].getSuit() + " and a "
+						+ deckCards[drawRand2].getFaceCard() + " of "
+						+ deckCards[drawRand2].getSuit() + " for a total of "
+						+ (deckCards[drawRand].getCardNumber() + deckCards[drawRand2].getCardNumber()));
+				deckCards[drawRand] = null;
+				deckCards[drawRand2] = null;
+			}
+			else{
+				System.out.println("Player " + (i + 1) + " has a "
 					+ deckCards[drawRand].getCardNumber() + " of "
 					+ deckCards[drawRand].getSuit() + " and a "
 					+ deckCards[drawRand2].getCardNumber() + " of "
@@ -51,6 +80,8 @@ public class BlackJack0{
 					+ (deckCards[drawRand].getCardNumber() + deckCards[drawRand2].getCardNumber()));
 			deckCards[drawRand] = null;
 			deckCards[drawRand2] = null;
+			}
 		}
+		deckCards = Cards.shuffle(deckCards);
 	}
 }
