d067ac6ed2f5d003419ad8f015d6aafc1d69e098
GoldAu <picard33470@sas.edu.sg>
2016-10-18 12:00:19 +0800

    Create Cards.java

--- Cards.java
+++ Cards.java
@@ -1,7 +1,28 @@
 public class Cards{
 	private int cardNumber;
 	private int cardSuit;
+	private String faceCard;
+	private boolean faceCardNumber = false;
 	public Cards(int x, int y){
+		if(x == 11){
+			faceCardNumber = true;
+			x = 10;
+			faceCard = "Jack";	
+		}
+		else if(x == 12){
+			faceCardNumber = true;
+			x = 10;
+			faceCard = "Queen";
+		}
+		else if(x == 13){
+			faceCardNumber = true;
+			x = 10;
+			faceCard = "King";
+		}
+		else{
+			cardNumber = x;
+			faceCard = "";
+		}
 		cardNumber = x;
 		cardSuit = y;
 	}
@@ -15,7 +36,33 @@ public class Cards{
 		else
 			return "Spades";
 	}
+	public String getFaceCard(){
+		if (faceCardNumber)
+			return faceCard;
+		else
+			return faceCard;
+	}
 	public int getCardNumber(){
-		return cardNumber;
+			return cardNumber;
+	}
+	public boolean isAFaceCard(){
+		return faceCardNumber;
+	}
+	public static Cards[] shuffle(Cards[] x){
+		Cards[] y = new Cards[x.length];
+		int suitCards = y.length / 4;
+		for(int i = 0; i < suitCards; i++){ //Initialize first suit of cards
+			y[i] = new Cards((i % 10) + 1, 0);
+		}
+		for(int i = 0; i < suitCards; i++){
+			y[i + suitCards] = new Cards((i % 10) + 1, 1);
+		}
+		for(int i = 0; i < suitCards; i++){
+			y[i + 2 * suitCards] = new Cards((i % 10) + 1, 2);
+		}
+		for(int i = 0; i < suitCards; i++){
+			y[i + 3*suitCards] = new Cards((i % 10) + 1, 3);
+		}
+		return y;
 	}
 }
